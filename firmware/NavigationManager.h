dff09850376e23a6aa489b2cc56c53751ca4879c
zakiadnan9506-alt <zakiadnan9506@gmail.com>
2026-07-01 22:37:48 +0700

    NavigationManager.h

--- firmware/NavigationManager.h
+++ firmware/NavigationManager.h
@@ -1,1 +1,39 @@
-// Placeholder - will be filled in subsequent steps.
+// Placeholder - will be #ifndef NAVIGATION_MANAGER_H
+#define NAVIGATION_MANAGER_H
+
+#include <Arduino.h>
+#include "Types.h"
+
+class NavigationManager
+{
+public:
+
+    NavigationManager();
+
+    void begin();
+
+    void reset();
+
+    void update();
+
+    void setPacket(const NavigationPacket &packet);
+
+    const NavigationState& state() const;
+
+    bool hasUpdate() const;
+
+    void clearUpdate();
+
+    void setConnected(bool connected);
+
+    bool isConnected() const;
+
+private:
+
+    NavigationState m_state;
+
+};
+
+extern NavigationManager navigation;
+
+#endiffilled in subsequent steps.
