c8d83706e9ed2e67871fb3e1d6f78205fac65bda
zakiadnan9506-alt <zakiadnan9506@gmail.com>
2026-07-01 22:36:24 +0700

    Types.h

--- firmware/Types.h
+++ firmware/Types.h
@@ -1,1 +1,96 @@
-// Placeholder - will be filled in subsequent steps.
+// Placeholder - will be filled in s#ifndef TYPES_H
+#define TYPES_H
+
+#include <Arduino.h>
+#include "config.h"
+
+/*
+====================================================
+Command
+====================================================
+*/
+
+enum class Command : uint8_t
+{
+    NONE = 0,
+    NAVIGATION = 1,
+    PING = 2,
+    SETTINGS = 3,
+    BATTERY = 4
+};
+
+/*
+====================================================
+Direction
+====================================================
+*/
+
+enum class Direction : uint8_t
+{
+    NONE = 0,
+
+    STRAIGHT,
+
+    LEFT,
+
+    RIGHT,
+
+    SLIGHT_LEFT,
+
+    SLIGHT_RIGHT,
+
+    SHARP_LEFT,
+
+    SHARP_RIGHT,
+
+    UTURN,
+
+    ROUNDABOUT,
+
+    ARRIVED
+};
+
+/*
+====================================================
+BLE Packet
+
+Ukuran tetap
+36 Byte
+====================================================
+*/
+
+#pragma pack(push,1)
+
+struct NavigationPacket
+{
+    uint8_t command;
+
+    uint8_t direction;
+
+    uint16_t distance;
+
+    char road[MAX_ROAD_NAME];
+};
+
+#pragma pack(pop)
+
+/*
+====================================================
+Navigation Runtime
+====================================================
+*/
+
+struct NavigationState
+{
+    Direction direction;
+
+    uint16_t distance;
+
+    char road[MAX_ROAD_NAME];
+
+    bool connected;
+
+    bool updated;
+};
+
+#endifubsequent steps.
