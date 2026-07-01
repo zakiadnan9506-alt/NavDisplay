e586d6f7a13efdd889a252aed2739dbd005626f0
zakiadnan9506-alt <zakiadnan9506@gmail.com>
2026-07-01 22:42:57 +0700

    DisplayManager.h

--- firmware/DisplayManager.h
+++ firmware/DisplayManager.h
@@ -1,1 +1,45 @@
 // Placeholder - will be filled in subsequent steps.
+#ifndef DISPLAY_MANAGER_H
+#define DISPLAY_MANAGER_H
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#include "config.h"
+#include "Types.h"
+
+class DisplayManager
+{
+public:
+
+    bool begin();
+
+    void splash();
+
+    void showWaiting();
+
+    void showNavigation(const NavigationState &state);
+
+    void update();
+
+private:
+
+    Adafruit_SSD1306 display =
+        Adafruit_SSD1306(
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            &Wire,
+            -1
+        );
+
+    void drawHeader(bool connected);
+
+    void drawDirection(Direction direction);
+
+};
+
+extern DisplayManager oled;
+
+#endif
