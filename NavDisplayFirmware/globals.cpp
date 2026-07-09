f444a3ac1c4fcc9ecbcc36dbf9c9cd4e814701b6
zakiadnan9506-alt <zakiadnan9506@gmail.com>
2026-07-09 12:52:26 +0700

    Update globals.cpp

--- NavDisplayFirmware/globals.cpp
+++ NavDisplayFirmware/globals.cpp
@@ -1,33 +1,10 @@
-/**************************************************************************
- *
- * NavDisplay Framework v1.0
- *
- * File : globals.cpp
- *
- * Description:
- * Global object definitions.
- *
- **************************************************************************/
-
 #include "globals.h"
 
-//
-// ==========================================================
-// GLOBAL OBJECTS
-// ==========================================================
-//
-// Semua object global hanya dideklarasikan satu kali
-// pada file ini.
-//
-// Inisialisasi dilakukan menggunakan default member
-// initializer yang telah didefinisikan pada globals.h,
-// sehingga tidak diperlukan constructor tambahan.
-//
-// ==========================================================
-//
+namespace NavDisplay
+{
 
 NavigationData navData{};
-
 BatteryData batteryData{};
-
 SystemStatus systemStatus{};
+
+} // namespace NavDisplay
